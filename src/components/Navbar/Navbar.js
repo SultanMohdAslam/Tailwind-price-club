f0c3debaf4033778a91c19d5f626436396047299
SultanMohdAslam <smaslam199320@gmail.com>
2022-10-06 00:31:35 +0600

    add tailwind project

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Navbar = () => {
+    const routes = [
+        { id: 1, name: 'Home', path: '/home' },
+        { id: 2, name: 'Products', path: '/Products' },
+        { id: 3, name: 'Orders', path: '/Orders' },
+        { id: 4, name: 'Contant', path: '/Contant' },
+        { id: 5, name: 'About', path: '/About' }
+    ]
+    return (
+        <nav>
+            <ul>
+
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
